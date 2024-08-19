6bb1e4ac1c2bd722966a117c2ecd308855ca9756
PrasannaRajanS <prasanna555001@gmail.com>
2024-08-19 18:03:45 +0530

    fetch:Section 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+import { NewTicketComponent } from './dashboard/tickets/new-ticket/new-ticket.component';
+import { TicketComponent } from './dashboard/tickets/ticket/ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ServerStatusComponent,
+    TicketsComponent,
+    TrafficComponent,
+    DashboardItemComponent,
+    NewTicketComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule,
